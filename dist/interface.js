581b8bf221d896498ab0d64d8b95ade0003401a2
hayet <habethayet@gmail.com>
2017-09-07 10:40:44 +0200

    tsc

---
+++ dist/interface.js
@@ -0,0 +1,21 @@
+var Ninja = /** @class */ (function () {
+    function Ninja() {
+    }
+    Ninja.prototype.attack = function (f) { };
+    Ninja.prototype.defend = function (f) { };
+    Ninja.prototype.heal = function (f) { };
+    Ninja.prototype.throw = function (f) { };
+    Ninja.prototype.jump = function (f) { };
+    return Ninja;
+}());
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.prototype.eat = function () { };
+    Person.prototype.move = function () { };
+    return Person;
+}());
+var n = new Ninja();
+var p = new Person();
+var f = p;
+//# sourceMappingURL=interface.js.map
---
+++ interface.ts
@@ -0,0 +1,21 @@
+interface Fighter{
+    attack(f :Fighter);
+    defend(f :Fighter);
+    treat (f : Fighter);
+}
+
+class Ninja {
+    attack(f :Fighter) {}
+    defend(f :Fighter) {}
+    heal(f :Fighter) {}
+    throw(f :Fighter) {}
+    jump(f :Fighter) {}
+}
+class Person {
+    eat() {}
+    move() {}
+
+}
+let n = new Ninja();
+let p = new Person();
+let f: Fighter = p;
